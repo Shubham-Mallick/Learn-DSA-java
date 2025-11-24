00506e29aed2d8765477b772e81b55766782da90
Shubham Mallick <124443747+Shubham-Mallick@users.noreply.github.com>
2025-11-24 23:03:54 +0530

    Corrcting file name

--- LinkedList/LinkedListNode.java
+++ LinkedList/LinkedListNode.java
@@ -1,11 +1,14 @@
 public class LinkedListNode {
-     static class Node {
-        int data;
-        Node next;
+    public static void main(String[] args) {
+        System.out.println("This is Node class");
+        Node node1 = new Node(10);
+        Node node2 = new Node(20);
 
-        Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
+        // Linking nodes
+        node1.next = node2;
+
+        System.out.println("Node data: " + node1.data);
+        System.out.println("Next node data: " + node1.next.data);
+
     }
 }
--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -1,14 +1,12 @@
 public class Node {
-    public static void main(String[] args) {
-        System.out.println("This is Node class");
-        LinkedListNode.Node node1 = new LinkedListNode.Node(10);
-        LinkedListNode.Node node2 = new LinkedListNode.Node(20);
+    int data;
+    Node next;
 
-        // Linking nodes
-        node1.next = node2;
-
-        System.out.println("Node data: " + node1.data);
-        System.out.println("Next node data: " + node1.next.data);
+    // Making Node blueprint
+    Node(int data){
+        this.data = data;
+        this.next = next;
 
     }
+    
 }
